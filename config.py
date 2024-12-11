e651417fc930346c7de755e4d032abed3a53fc11
ak4422920 <ak4422920@gmail.com>
2024-12-11 09:21:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://cine:cine@cluster0.w6hlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668 1049173662').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
