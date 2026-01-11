d26f0f93b41f8b4abd3e4bb1bb34f547ced89158
ak4422920 <ak4422920@gmail.com>
2026-01-11 06:26:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class Config:
     API_ID = environ.get("API_ID", "29171167")
     API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Akforwardbot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "@Boa404bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://for:for@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190352334'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/cineoriginals") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5482682830').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002416220645'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/AkmovieVerse") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
